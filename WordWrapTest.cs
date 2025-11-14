6faa4f66dda0d3ff8cb125d8990aac763c343d88
SantisanR2 <sa.ramirezr@uniandes.edu.co>
2025-11-14 09:56:18 -0500

    🟥 Cuando_IngresoWordWordCon5Columnas_Debe_RegresarWord_Word

--- WordWrapTest.cs
+++ WordWrapTest.cs
@@ -52,6 +52,14 @@ public class WordWrapTest
         result.Should().Be("word\nword");
     }    
     
+    [Fact]
+    public void Cuando_IngresoWordWordCon5Columnas_Debe_RegresarWord_Word()
+    {
+        var result = Wrap("word word", 5);
+
+        result.Should().Be("word\nword");
+    }
+    
     private static string Wrap(string text, int col)
     {
         if (string.IsNullOrEmpty(text)) return text;
